f411f156ee181b07c0fb7340ecffe0773107b0d4
Daniil <dadyarri@gmail.com>
2020-04-07 13:46:59 +0300

    Use Markdown v2 by default.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ TG_TOKEN = environ["TG_TOKEN"]
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=TG_TOKEN)
+bot = Bot(token=TG_TOKEN, parse_mode=types.ParseMode.MARKDOWN_V2)
 dp = Dispatcher(bot)
 
 
@@ -23,16 +23,12 @@ async def send_welcome(message: types.Message):
 
     welcome_message = REPLICAS["welcome"].format(message.from_user["first_name"])
     keyboard = generate_main_menu(is_admin(message.from_user["id"]))
-    await message.answer(
-        welcome_message, parse_mode=types.ParseMode.MARKDOWN, reply_markup=keyboard
-    )
+    await message.answer(welcome_message, reply_markup=keyboard)
 
 
 @dp.message_handler(commands=["help"])
 async def send_help(message: types.Message):
-    await message.answer(
-        REPLICAS["help"], parse_mode=types.ParseMode.MARKDOWN,
-    )
+    await message.answer(REPLICAS["help"])
 
 
 @dp.message_handler()
